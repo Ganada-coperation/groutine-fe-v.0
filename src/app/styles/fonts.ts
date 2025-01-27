8f4ef7140b385160400801075e8b53ca2039ce76
Tnalxmsk <alstnwkd990@naver.com>
2025-01-27 16:13:27 +0900

    init: font set

--- src/app/styles/fonts.ts
+++ src/app/styles/fonts.ts
@@ -9,8 +9,9 @@ const createFontStyle = (family: string, weight: number, size: number, lineHeigh
 `;
 
 export const fonts: FontsTypes = {
-  /*heading_bold_24px: createFontStyle('Pretendard', 700, 24, 34),
+  heading_bold_24px: createFontStyle('Pretendard', 700, 24, 34),
   heading_bold_22px: createFontStyle('Pretendard', 700, 22, 34),
+  heading_bold_18px: createFontStyle('Pretendard', 700, 18, 34),
   heading_medium_20px: createFontStyle('Pretendard', 500, 20, 34),
   heading_medium_18px: createFontStyle('Pretendard', 500, 18, 34),
   body_bold_16px: createFontStyle('Pretendard', 700, 16, 24),
@@ -19,5 +20,6 @@ export const fonts: FontsTypes = {
   caption_bold_14px: createFontStyle('Pretendard', 700, 14, 24),
   caption_medium_14px: createFontStyle('Pretendard', 500, 14, 24),
   detail_medium_12px: createFontStyle('Pretendard', 500, 12, 22),
-  button_medium_16px: createFontStyle('Pretendard', 500, 16, 24),*/
+  detail_medium_10px: createFontStyle('Pretendard', 500, 10, 22),
+  button_medium_16px: createFontStyle('Pretendard', 500, 16, 24),
 };

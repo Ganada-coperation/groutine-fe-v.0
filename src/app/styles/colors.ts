57538e6cc528d6b3f888f430b542a0941860b412
Tnalxmsk <alstnwkd990@naver.com>
2025-01-27 16:11:48 +0900

    init: color set

--- src/app/styles/colors.ts
+++ src/app/styles/colors.ts
@@ -1,5 +1,18 @@
 import { ColorsTypes } from '@shared/types';
 
 export const colors: ColorsTypes = {
-
+  darkGray: '#4F4F4F',
+  mediumGray: '#bdbdbd',
+  lightGray: '#eaeaea',
+  darkestPrimary: '#011F2B',
+  defaultPrimary: '#02343F',
+  lighterPrimary: '#356778',
+  lightestPrimary: '#688D99',
+  lightestestPrimary: '#CCD4D3',
+  red: '#FF6B6B',
+  darkestSecondary: '#E6DED2',
+  darkerSecondary: '#F2E7D9',
+  defaultSecondary: '#FFF6EA',
+  lighterSecondary: '#FFF9F1',
+  lightestSecondary: '#FFFCF8',
 };

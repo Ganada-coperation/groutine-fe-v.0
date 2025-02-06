551f62bdb9dd711628b1e8da4ae47df16bb566d1
Tnalxmsk <alstnwkd990@naver.com>
2025-02-06 19:08:06 +0900

    :sparkles: feat: index 파일 로그인 페이지, 로그인 스키마 추가

--- src/pages/auth/features/index.ts
+++ src/pages/auth/features/index.ts
@@ -2,3 +2,4 @@ export * from './utils/validSignUp.ts';
 export * from './utils/useCustomForm.ts';
 export * from './utils/useValidation.ts';
 export * from './schema/signupSchema.ts';
+export * from './schema/signInSchema.ts';

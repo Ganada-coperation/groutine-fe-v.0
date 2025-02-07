f9b2ff2396bbfa7e35a6ed438e6ac8206cfd9a67
Tnalxmsk <alstnwkd990@naver.com>
2025-02-07 15:53:06 +0900

    :sparkles: feat: 로그인 스키마 추가

--- src/pages/auth/features/schema/signInSchema.ts
+++ src/pages/auth/features/schema/signInSchema.ts
@@ -5,11 +5,14 @@ export interface InputSignInValue extends Pick<InputValue, 'email' | 'password'>
 }
 
 export const signInSchema = z.object({
-  email: z.string().email(),
+  email: z
+    .string()
+    .email('올바르지 않은 이메일 형식입니다.')
+    .max(50, '이메일은 최대 50자까지 가능합니다.'),
   password: z
     .string()
     .min(8, '비밀번호는 최소 8자 이상이어야 합니다.')
-    .max(20, '비밀번호는 최대 20자까지 가능합니다.')
+    .max(10, '비밀번호는 최대 20자까지 가능합니다.')
     .regex(/[0-9]/, '비밀번호는 최소 1개의 숫자를 포함해야 합니다.')
     .regex(/[^A-Za-z0-9]/, '비밀번호는 최소 1개의 특수 문자를 포함해야 합니다.'),
 });

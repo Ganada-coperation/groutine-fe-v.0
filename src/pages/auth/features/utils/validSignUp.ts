b13a3458d91c159eaaefac9aa984cebde9dc5484
Tnalxmsk <alstnwkd990@naver.com>
2025-02-07 15:54:21 +0900

    :recycle: refactor: date 추가

--- src/pages/auth/features/utils/validSignUp.ts
+++ src/pages/auth/features/utils/validSignUp.ts
@@ -7,6 +7,7 @@ export const validSignUp = (watch: UseFormWatch<InputValue>) => {
   const passwordValue = watch('password', '');
   const passwordConfirmValue = watch('passwordConfirm', '');
   const nicknameValue = watch('nickname', '');
+  const dateValue = watch('date', '');
 
   const isEmailValid = signUpSchema.shape.email.safeParse(emailValue).success;
   const isCodeValid = signUpSchema.shape.code.safeParse(codeValue).success;
@@ -14,5 +15,16 @@ export const validSignUp = (watch: UseFormWatch<InputValue>) => {
   const isPasswordConfirmValid = signUpSchema.shape.passwordConfirm.safeParse(passwordConfirmValue).success;
   const isNicknameValid = signUpSchema.shape.nickname.safeParse(nicknameValue).success;
 
-  return { isEmailValid, isCodeValid, isPasswordValid, isPasswordConfirmValid, isNicknameValid };
+  const equalPassword: boolean = passwordValue === passwordConfirmValue;
+
+  const formatDate = () => {
+    if (!dateValue) return '';
+    const date = new Date(dateValue);
+    const year = `${date.getFullYear()}년`;
+    const month = `${date.getMonth() + 1}월`;
+    const day = `${date.getDate()}일`;
+    return `${year} ${month} ${day}`;
+  }
+
+  return { isEmailValid, isCodeValid, isPasswordValid, isPasswordConfirmValid, isNicknameValid, equalPassword, formatDate, dateValue };
 };

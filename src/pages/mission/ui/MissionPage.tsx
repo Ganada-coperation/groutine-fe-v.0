284524fbea371701bd9a9d2b7b0f3989e9638bed
Tnalxmsk <alstnwkd990@naver.com>
2025-02-11 01:44:50 +0900

    :art: style: MissionPage UI 구현

--- src/pages/mission/ui/MissionPage.tsx
+++ src/pages/mission/ui/MissionPage.tsx
@@ -1,18 +1,34 @@
 import Banner from "@mission/components/Banner.tsx";
 import TodayMission from "@mission/components/TodayMission.tsx";
 import ChallengeList from "@mission/components/ChallengeList.tsx";
+import styled from "styled-components";
 
 export const MissionPage = () => {
-  // 배너 - 배너 컴포넌트
-  // 오늘의 미션 - 오늘의 미션 컴포넌트 (미션 이미지, 미션 이름, 미션하기 버튼) <- 미션 컴포넌트 분리
-  // 도전 중인 챌린지 - 컴포넌트, 챌린지 아이템 컴포넌트
 
   return (
-    <>
+    <Container>
       <Banner />
-      <TodayMission />
-      <ChallengeList />
-    </>
+      <Wrapper>
+        <TodayMission />
+        <ChallengeList />
+      </Wrapper>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.lighterSecondary};
+  overflow-x: hidden;
+  overflow-y: auto;
+  padding-bottom: 20px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 0 20px;
+  margin-top: 20px;
+  gap: 20px;
+`;
 

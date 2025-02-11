8e7dd3035d87e3c0acc983e816bf6466da4c66ad
heyn2 <hy702@gachon.ac.kr>
2025-02-11 19:39:27 +0900

    :art: style: 반응형 구현(react-responsive)

--- src/pages/home/components/Tabs.tsx
+++ src/pages/home/components/Tabs.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import styled from "styled-components";
+import { useMediaQuery } from 'react-responsive';
 
 const Tabs: React.FC = () => {
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1199 });
+  const isDesktop = useMediaQuery({ minWidth: 1200 });
+
+  // 헤더 높이에 맞게 탭 위치 조정
+  let topOffset = isMobile ? '200px' : isTablet ? '250px' : '260px';
+
   return (
-    <TabsContainer>
+    <TabsContainer style={{ top: topOffset }}>
       <TabButton className="active">최신순</TabButton>
       <TabButton>인기순</TabButton>
     </TabsContainer>
@@ -12,43 +20,35 @@ const Tabs: React.FC = () => {
 
 export default Tabs;
 
-
 const TabsContainer = styled.div`
+position: sticky;
+  width: 100%;
+  max-width: 768px; /* 부모 컨테이너를 벗어나지 않도록 제한 */
+  margin: 0 auto;
+  background-color: white;
   display: flex;
   justify-content: flex-start;
-  margin: 0;
-  padding: 20px;
-
-  @media (max-width: 767px) {
-    padding: 10px; /* 모바일 */
-    flex-wrap: wrap;
-  }
+  padding: 15px 20px;
+  z-index: 999;
 `;
 
-
 const TabButton = styled.button`
-  padding: 10px 20px;
   margin-right: 10px;
+  padding: 6px 10px;
   border: none;
   border-radius: 50px;
-  background-color: #f9f6f2;
+  background-color: #ccd9dd;
   color: #02343f;
   cursor: pointer;
-  font-size: 16px;
   transition: background-color 0.2s ease-in-out, color 0.2s ease-in-out;
 
   &.active {
     background-color: #02343f;
-    color: #f9f6f2;
+    color: #ccd9dd;
   }
 
   &:hover {
     background-color: #011f26;
     color: #ffffff;
-  }
-
-  @media (max-width: 767px) {
-    padding: 8px 16px;
-    font-size: 14px; /* 모바일 */
   }
 `;

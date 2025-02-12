a21c42f1e6eb48f114fa6cc5178e797a9c20fc57
Tnalxmsk <alstnwkd990@naver.com>
2025-02-12 23:35:53 +0900

    :recycle: refactor: 버튼 타입 수정 및 계층 변경
    버튼 스타일 관련 타입 프로퍼티 추가
    pages/ui/components -> shared/ui 변경

--- src/shared/ui/CustomButton.tsx
+++ src/shared/ui/CustomButton.tsx
@@ -1,21 +1,27 @@
 import styled from "styled-components";
 import { ActiveProps } from "@shared/types";
 
+
 export interface ButtonProps extends ActiveProps {
   disabled?: boolean;
   onClick?: () => void;
   label: string;
+  padding?: "12px" | "18px";
 }
 
-export const CustomButton = ({ $isActive, disabled, onClick, label  }: ButtonProps) => {
-  return <Button $isActive={$isActive} disabled={disabled} onClick={onClick}>{label}</Button>;
+export const CustomButton = ({ $isActive, disabled, onClick, label, padding  }: ButtonProps) => {
+  return <Button $isActive={$isActive} $padding={padding} disabled={disabled} onClick={onClick}>{label}</Button>;
 }
 
-const Button = styled.button<ActiveProps>`
+interface ButtonStyleProps extends ActiveProps {
+  $padding?: "12px" | "18px";
+}
+
+const Button = styled.button<ButtonStyleProps>`
   width: 100%;
   background-color: ${({ theme, $isActive }) => $isActive ? theme.colors.defaultPrimary : theme.colors.darkerSecondary};
   color: ${({ theme, $isActive }) => $isActive ? theme.colors.defaultSecondary : theme.colors.mediumGray};
   font: ${({ theme }) => theme.fonts.button_medium_16px};
   border-radius: 10px;
-  padding: 18px 0;
+  padding: ${({ $padding }) => $padding ? `${$padding} 0` : `18px 0`};
 `;

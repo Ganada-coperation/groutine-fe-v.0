8d34a4fbe5897a111dc4735f82038f65c5380a15
Tnalxmsk <alstnwkd990@naver.com>
2025-02-13 16:50:50 +0900

    :art: style: 가이드 메세지 애니메이션 효과 추가

--- src/pages/mission/components/certification/UploadGuide.tsx
+++ src/pages/mission/components/certification/UploadGuide.tsx
@@ -3,13 +3,14 @@ import IcQuestion from "@icon/ic-question.svg";
 import IcArrowBottom from "@icon/ic-arrow-bottom.svg";
 import IcArrowTop from "@icon/ic-arrow-top.svg";
 import { useState } from "react";
+import { motion, AnimatePresence } from 'framer-motion';
 
 const UploadGuide = () => {
   const [visible, setVisible] = useState<boolean>(false);
 
   const changeVisible = () => {
     setVisible(!visible);
-  }
+  };
 
   return (
     <UploadGuideContainer>
@@ -19,12 +20,19 @@ const UploadGuide = () => {
         <p>어떻게 인증해야 하나요?</p>
         <img src={visible ? IcArrowTop : IcArrowBottom} alt="" onClick={changeVisible} />
       </CertificationGuide>
-      {visible && (
-        <Guide>
-          <p><span>{'IOS 가이드\n'}</span>{'설정 > 스크린타임 > 앱 및 웹 사이트 활동 모두 보기\n > 일일 스크린타임 시간 캡처'}</p>
-          <p><span>{'IOS 가이드\n'}</span>{'설정 > 디지털 웰빙 및 자녀 보호 기능 > 그래프 클릭\n > 일일 사용현황 캡처'}</p>
-        </Guide>
-      )}
+      <AnimatePresence>
+        {visible && (
+          <Guide
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: 'auto', opacity: 1 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.4 }}
+          >
+            <p><span>{'IOS 가이드\n'}</span>{'설정 > 스크린타임 > 앱 및 웹 사이트 활동 모두 보기\n > 일일 스크린타임 시간 캡처'}</p>
+            <p><span>{'IOS 가이드\n'}</span>{'설정 > 디지털 웰빙 및 자녀 보호 기능 > 그래프 클릭\n > 일일 사용현황 캡처'}</p>
+          </Guide>
+        )}
+      </AnimatePresence>
     </UploadGuideContainer>
   );
 };
@@ -53,12 +61,14 @@ const CertificationGuide = styled.div`
   }
 `;
 
-const Guide = styled.div`
+
+const Guide = styled(motion.div)`
+  overflow: hidden;
   display: flex;
   flex-direction: column;
   gap: 15px;
   padding: 0 20px;
-  
+
   span {
     text-decoration: underline;
     font: ${({ theme }) => theme.fonts.detail_regular_14px};

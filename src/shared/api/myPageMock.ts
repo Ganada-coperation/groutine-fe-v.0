c862118b5528a8419189ee8c3654f6eff70bcd51
heyn2 <hy702@gachon.ac.kr>
2025-02-20 22:27:08 +0900

    :sparkles: feat: 마이페이지 관련 mock data 정의

---
+++ src/shared/api/myPageMock.ts
@@ -0,0 +1,28 @@
+import ImgExample from "@img/img-example.png"
+import { Challenge } from "@shared/types/Challenge";
+
+export const ongoingChallenges: Challenge[] = [
+  { id: 1, title: '불타오르는 유산소🔥', startDate: '2024.06.01', endDate: '2024.08.02', daysLeft: 5, image: ImgExample },
+  { id: 2, title: '불타오르는 유산소🔥', startDate: '2024.06.01', endDate: '2024.08.02', daysLeft: 5, image: ImgExample },
+  { id: 3, title: '불타오르는 유산소🔥', startDate: '2024.06.01', endDate: '2024.08.02', daysLeft: 5, image: ImgExample },
+];
+
+export const appliedChallenges: Challenge[] = [
+  { id: 4, title: '신청한 챌린지 1', startDate: '2024.06.01', endDate: '2024.08.02', daysLeft: 5, image: ImgExample },
+  { id: 5, title: '신청한 챌린지 2', startDate: '2024.06.01', endDate: '2024.08.02', daysLeft: 5, image: ImgExample },
+];
+
+export const completedChallenges: Challenge[] = [
+  { id: 6, title: '완료된 챌린지 1', startDate: '2024.06.01', endDate: '2024.08.02', participants: 99, completionRate: 100, image: ImgExample },
+  { id: 7, title: '완료된 챌린지 2', startDate: '2024.06.01', endDate: '2024.08.02', participants: 99, completionRate: 100, image: ImgExample },
+  { id: 8, title: '완료된 챌린지 3', startDate: '2024.06.01', endDate: '2024.08.02', participants: 99, completionRate: 100, image: ImgExample },
+  { id: 9, title: '완료된 챌린지 4', startDate: '2024.06.01', endDate: '2024.08.02', participants: 99, completionRate: 100, image: ImgExample },
+];
+
+export const getUserChallenges = () => {
+  return {
+    ongoingChallenges,
+    appliedChallenges,
+    completedChallenges
+  };
+};
